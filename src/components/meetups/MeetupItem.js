60c84d4160f39d4dc42b2b8395681a75c55c1b88
ishitarastogi <ishita3099@gmail.com>
2021-10-26 12:01:17 +0530

    Add another approach to list items

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -4,12 +4,12 @@ function MeetupItem(props) {
   return (
     <li className={classes.item}>
       <div className={classes.image}>
-        <img src={props.image} alt={props.title} />
+        <img src={props.meetup.image} alt={props.meetup.title} />
       </div>
       <div className={classes.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
+        <h3>{props.meetup.title}</h3>
+        <address>{props.meetup.address}</address>
+        <p>{props.meetup.description}</p>
       </div>
       <div className={classes.actions}>
         <button>To Favorites</button>
@@ -19,3 +19,21 @@ function MeetupItem(props) {
 }
 
 export default MeetupItem;
+
+// function MeetupItem(props) {
+//     return (
+//       <li className={classes.item}>
+//         <div className={classes.image}>
+//           <img src={props.image} alt={props.title} />
+//         </div>
+//         <div className={classes.content}>
+//           <h3>{props.title}</h3>
+//           <address>{props.address}</address>
+//           <p>{props.description}</p>
+//         </div>
+//         <div className={classes.actions}>
+//           <button>To Favorites</button>
+//         </div>
+//       </li>
+//     );
+//   }
--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -5,13 +5,13 @@ function MeetupList(props) {
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
-        <MeetupItem
-          key={meetup.id}
-          id={meetup.id}
-          image={meetup.image}
-          title={meetup.title}
-          address={meetup.address}
-          description={meetup.description}
+        <MeetupItem meetup={meetup}
+        //   key={meetup.id}
+        //   id={meetup.id}
+        //   image={meetup.image}
+        //   title={meetup.title}
+        //   address={meetup.address}
+        //   description={meetup.description}
         />
       ))}
     </ul>
